65620cab8310b9783703ad27497c00e775272fa0
derivalent <you@example.com>
2024-11-04 14:22:08 +0700

    Sudah bimbingan 1, revisi

--- resources/views/public/dashboard.blade.php
+++ resources/views/public/dashboard.blade.php
@@ -1,6 +1,5 @@
 @extends('public/layout/main')
 @section('content')
-
     <main class="container">
         <div class="row gx-4 gx-lg-5 align-items-center my-4">
             <div class="col-lg-5" style="padding-left: 3%; padding-top: 1rem;">
@@ -48,7 +47,7 @@
                     Peraturan Bupati Banyuwangi dan Tentang Kedudukan, Susunan Organisasi, Tugas, Pokok dan Fungsi Serta
                     Tata Kerja Dinas Pemadam Kebakaran dan Penyelamatan Kabupaten Banyuwangi.</p>
                 <a class="btn btn-primary"
-                    href="https://spm.banyuwangikab.go.id/skpd/dinas-pemadam-kebakaran-dan-penyelamatan">Selengkapnya..</a>
+                    href="https://spm.banyuwangikab.go.id/skpd/dinas-pemadam-kebakaran-dan-penyelamatan-kabupaten-banyuwangi">Selengkapnya..</a>
             </div>
         </div>
 
@@ -62,8 +61,8 @@
         <div class="btn btn-danger mt-3"
             style="width: 100%; margin-bottom: 50px; margin-top: -30px; position: relative; box-shadow: 0 9px 9px rgba(0, 0, 0, 0.1);">
             <p class="text-white m-0 my-1 py-1">
-            <h2><a class="nav-link"
-                    href="https://wa.me/+6285748449996?text=LAPOR%20KEJADIAN!!!%0ASetelah%0Aberkomunikasi%0Avia%0Atelepon,%0Aharap%0Akirimkan%0Atitik%0Alokasi%0Akejadian.">
+            <h2><a class="nav-link" {{-- href="https://wa.me/+6285748449996?text=LAPOR%20KEJADIAN!!!%0ASetelah%0Aberkomunikasi%0Avia%0Atelepon,%0Aharap%0Akirimkan%0Atitik%0Alokasi%0Akejadian."> --}}
+                    href="https://wa.me/+6282211312113?text=LAPOR%20KEJADIAN!!!%0ASetelah%0Aberkomunikasi%0Avia%0Atelepon,%0Aharap%0Akirimkan%0Atitik%0Alokasi%0Akejadian.">
                     <b> LAYANAN PELAPORAN!!! </b></a></h2>
             </p>
         </div>
@@ -91,85 +90,88 @@
 
         </div>
         <section class="section_public">
-        <div class="row">
-            <div class="col-md-7 mt-4 mb-4" style="height: 350px;">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <iframe src="https://spm.banyuwangikab.go.id/skpd/dinas-pemadam-kebakaran-dan-penyelamatan"
-                            style="width: 100%; height: 100%; border: none;"></iframe>
+            <div class="row">
+                <div class="col-md-7 mt-4 mb-4" style="height: 350px;">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            {{-- <iframe src="https://spm.banyuwangikab.go.id/skpd/dinas-pemadam-kebakaran-dan-penyelamatan"
+                            style="width: 100%; height: 100%; border: none;"></iframe> --}}
+                            <iframe
+                                src="https://spm.banyuwangikab.go.id/skpd/dinas-pemadam-kebakaran-dan-penyelamatan-kabupaten-banyuwangi"
+                                style="width: 100%; height: 100%; border: none;"></iframe>
+                        </div>
                     </div>
                 </div>
+                <!-- Maps -->
+                <div class="col-md-5 mt-4 mb-4" style="height: 350px;">
+                    <div id="map" style="width: 100%; height: 100%; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);"></div>
+                    <script>
+                        //Tampilan Maps
+                        const map = L.map('map').setView([-8.234256388870639, 114.29139328370496], 9);
+                        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        }).addTo(map);
+
+                        //Marker
+                        //Lokasi 1
+                        var lokasi1 = L.icon({
+                            iconUrl: 'images/logo_damkar.png',
+                            iconSize: [65, 40], // size of the icon
+                        });
+
+                        L.marker([-8.21124339808114, 114.37976274943635], {
+                            icon: lokasi1
+                        }).addTo(map).bindPopup("<b>MAKO DAMKAR BANYUWANGI</b><br>Telp: (0333) 422113").openPopup();
+
+                        //Lokasi 2
+                        var lokasi2 = L.icon({
+                            iconUrl: 'images/logo_damkar.png',
+                            iconSize: [65, 40], // size of the icon
+                        });
+
+                        L.marker([-8.397562413652105, 114.26973405885026], {
+                            icon: lokasi2
+                        }).addTo(map).bindPopup("<b>SEKTOR SRONO</b><br>Srono")
+
+                        //Lokasi 3
+                        var lokasi3 = L.icon({
+                            iconUrl: 'images/logo_damkar.png',
+                            iconSize: [65, 40], // size of the icon
+                        });
+
+                        L.marker([-8.361345425593777, 114.15939366898164], {
+                            icon: lokasi3
+                        }).addTo(map).bindPopup("<b>SEKTOR GENTENG</b><br>Genteng")
+
+                        //Lokasi 4
+                        var lokasi4 = L.icon({
+                            iconUrl: 'images/logo_damkar.png',
+                            iconSize: [65, 40], // size of the icon
+                        });
+
+                        L.marker([-8.48352765424662, 114.13341408000693], {
+                            icon: lokasi4
+                        }).addTo(map).bindPopup("<b>SEKTOR BANGOREJO</b><br>Bangorejo")
+
+                        //Tampilan Maps Shareloc
+                        const maps = L.map('map2').setView([-8.234256388870639, 114.29139328370496], 9);
+                        const tile = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        }).addTo(map);
+
+                        //Lokasi 3
+                        var sharel = L.icon({
+                            iconUrl: './assets/image/logo_damkar.png',
+                            iconSize: [65, 40], // size of the icon
+                        });
+
+                        L.marker([-8.361345425593777, 114.15939366898164], {
+                            icon: lokasi3
+                        }).addTo(map).bindPopup("<b>SEKTOR GENTENG</b><br>Genteng")
+                    </script>
+                </div>
             </div>
-            <!-- Maps -->
-            <div class="col-md-5 mt-4 mb-4" style="height: 350px;">
-                <div id="map" style="width: 100%; height: 100%; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);"></div>
-                <script>
-                    //Tampilan Maps
-                    const map = L.map('map').setView([-8.234256388870639, 114.29139328370496], 9);
-                    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                    }).addTo(map);
-
-                    //Marker
-                    //Lokasi 1
-                    var lokasi1 = L.icon({
-                        iconUrl: 'images/logo_damkar.png',
-                        iconSize: [65, 40], // size of the icon
-                    });
-
-                    L.marker([-8.21124339808114, 114.37976274943635], {
-                        icon: lokasi1
-                    }).addTo(map).bindPopup("<b>MAKO DAMKAR BANYUWANGI</b><br>Telp: (0333) 422113").openPopup();
-
-                    //Lokasi 2
-                    var lokasi2 = L.icon({
-                        iconUrl: 'images/logo_damkar.png',
-                        iconSize: [65, 40], // size of the icon
-                    });
-
-                    L.marker([-8.397562413652105, 114.26973405885026], {
-                        icon: lokasi2
-                    }).addTo(map).bindPopup("<b>SEKTOR SRONO</b><br>Srono")
-
-                    //Lokasi 3
-                    var lokasi3 = L.icon({
-                        iconUrl: 'images/logo_damkar.png',
-                        iconSize: [65, 40], // size of the icon
-                    });
-
-                    L.marker([-8.361345425593777, 114.15939366898164], {
-                        icon: lokasi3
-                    }).addTo(map).bindPopup("<b>SEKTOR GENTENG</b><br>Genteng")
-
-                    //Lokasi 4
-                    var lokasi4 = L.icon({
-                        iconUrl: 'images/logo_damkar.png',
-                        iconSize: [65, 40], // size of the icon
-                    });
-
-                    L.marker([-8.48352765424662, 114.13341408000693], {
-                        icon: lokasi4
-                    }).addTo(map).bindPopup("<b>SEKTOR BANGOREJO</b><br>Bangorejo")
-
-                    //Tampilan Maps Shareloc
-                    const maps = L.map('map2').setView([-8.234256388870639, 114.29139328370496], 9);
-                    const tile = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                    }).addTo(map);
-
-                    //Lokasi 3
-                    var sharel = L.icon({
-                        iconUrl: './assets/image/logo_damkar.png',
-                        iconSize: [65, 40], // size of the icon
-                    });
-
-                    L.marker([-8.361345425593777, 114.15939366898164], {
-                        icon: lokasi3
-                    }).addTo(map).bindPopup("<b>SEKTOR GENTENG</b><br>Genteng")
-                </script>
-            </div>
-        </div>
-    </section>
+        </section>
         <div class="container-fluid px-4">
             <center>
                 <h3 class="mt-4 mb-4"><b>GRAFIK KEJADIAN</b></h3>
@@ -208,6 +210,8 @@
                             <button type="submit">Select Year</button>
                         </form> --}}
 
+
+                        {{-- grafik --}}
                         <form method="POST" action="{{ route('public.selectYear') }}" class="mb-3">
                             @csrf
                             <div class="form-group">
@@ -239,7 +243,7 @@
         </div>
     </main>
 
-   
+
 
     {{-- //padahal tadi siang bisa --}}
     {{-- <script>
